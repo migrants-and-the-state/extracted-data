1f4ab60883b1e9c5fd681dcefa55da184958cc84
mnyrop <marii@nyu.edu>
2025-01-17 13:46:20 -0500

    tidy up and validate schemas

--- lib/validate_json_to_schema.py
+++ lib/validate_json_to_schema.py
@@ -4,19 +4,28 @@ import json
 import jsonschema
 import os
 
-schema_path = './schemas/document-schema.json'
-json_paths  = glob.glob('./data/metadata_outputs/*.json')
-schema      = json.load(open(schema_path))
-validator   = jsonschema.Draft202012Validator(schema)
+def validate_record(json_path, validator):
+  with open(json_path, 'r') as fp:
+    test_json = json.load(fp)
+    valid = validator.is_valid(test_json)
+    if not valid:
+      print(f'{json_path} failed!') 
+      print(validator.validate(test_json))
+    return valid
+  
+def validate_records(json_paths, schema_path):
+  schema = json.load(open(schema_path))
+  with alive_progress.alive_bar(len(json_paths), title='validating...', spinner='waves') as bar:
+    validator = jsonschema.Draft202012Validator(schema)
+    for json_path in json_paths:
+      validate_record(json_path, validator)
+      bar() 
 
-with alive_progress.alive_bar(len(json_paths), title='validating...', spinner='waves') as bar:
-  for json_path in json_paths:
-    with open(json_path, 'r') as fp:
-      filename  = os.path.basename(json_path)
-      test_json = json.load(fp)
-      valid     = validator.is_valid(test_json)
+afile_schema_path = './schemas/afile-schema-kc-2023.json'
+afile_json_paths  = glob.glob('./data/afiles/*.json')
+page_schema_path = './schemas/page-schema-kc-2023.json'
+page_json_paths  = glob.glob('./data/pages/*.json')
 
-      if not valid:
-        print(f'{filename} failed!') 
-        print(validator.validate(test_json))
-      bar()  
+validate_records(afile_json_paths, afile_schema_path)
+validate_records(page_json_paths, page_schema_path)
+
